1927da4108ae9b2191ff8da0b94248d95df916de
unknown <bhags268@gmail.com>
2017-05-18 14:15:17 +0530

    First Commit

---
+++ www/js/app.js
@@ -0,0 +1,121 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(function($stateProvider,$urlRouterProvider) {
+   
+
+    $stateProvider.state('changePassword', {
+        url: '/changePassword',
+        templateUrl: 'templates/changePassword.html'
+
+    })
+     .state('addBalance', {
+        url: '/addBalance',
+        templateUrl: 'templates/addBalance.html'
+
+    })
+    .state('addRetailer', {
+        url: '/addRetailer',
+        templateUrl: 'templates/addRetailer.html'
+
+    })
+     .state('currentBalance', {
+        url: '/currentBalance',
+        templateUrl: 'templates/currentBalance.html'
+
+    })
+     .state('complain', {
+        url: '/complain',
+        templateUrl: 'templates/complain.html'
+
+    })
+     .state('dthRecharge', {
+        url: '/dthRecharge',
+        templateUrl: 'templates/dthRecharge.html'
+
+    })
+    .state('login', {
+        url: '/login',
+        controller:'loginCtrl',
+        templateUrl: 'templates/login.html'
+
+    })
+    .state('masterDealerHome', {
+        url: '/masterDealerHome',
+        controller:'masterDelearCtrl',
+        templateUrl: 'templates/mastreDealerHome.html'
+
+
+    })
+    .state('mobileRecharge', {
+        url: '/mobileRecharge',
+        templateUrl: 'templates/mobileRecharge.html'
+
+    })
+    .state('postPaidRecharge', {
+        url: '/postPaidRecharge',
+        templateUrl: 'templates/postPaidRecharge.html'
+
+    })
+    .state('recharge', {
+        url: '/recharge',
+        templateUrl: 'templates/recharge.html'
+
+    })
+    .state('rechargeReport', {
+        url: '/rechargeReport',
+        templateUrl: 'templates/rechargeReport.html'
+
+    })
+    .state('report', {
+        url: '/report',
+        templateUrl: 'templates/reports.html',
+        controller:'reportCtrl'
+    })
+     .state('retailerHome', {
+        url: '/retailerHome',
+        templateUrl: 'templates/retailerHome.html'
+
+    })
+    .state('revertBalance', {
+        url: '/revertBalance',
+        templateUrl: 'templates/revertBalance.html'
+
+    })
+    .state('searchTransaction', {
+        url: '/searchTransaction',
+        templateUrl: 'templates/searchTransaction.html'
+
+    })
+    .state('delearHome', {
+        url: '/delearHome',
+        templateUrl: 'templates/delearHome.html',
+       controller:'delearCtrl'
+    })
+    ;
+      
+    
+   
+  $urlRouterProvider.otherwise('login');
+})
---
+++ www/js/controllers.js
@@ -0,0 +1,196 @@
+angular.module('starter.controllers', [])
+.controller('loginCtrl' ,function($scope,$state){
+$scope.login=function(){
+   console.log($scope.userName);
+   console.log($scope.password);
+   if($scope.userName==100637 && $scope.password==88848){
+       $state.go('retailerHome');
+   }else if($scope.userName==2005 && $scope.password==88848){
+       $state.go('delearHome');
+   }else{
+       $state.go('masterDealerHome');
+   }
+}
+
+
+}) 
+.controller('masterDelearCtrl' ,function($scope,$state){
+$scope.masterDelear=[];
+
+$scope.masterDelear=
+[
+{name:"Current Balance",clas:'icon ionIcon ion-cash'},
+{name:"Complain",clas:'icon ionIcon ion-ios-email'},
+{name:"Complain List",clas:'icon ionIcon ion-ios-email'},
+{name:"Change Password",clas:'icon ionIcon ion-key'},
+{name:"Reports",clas:'icon ionIcon ion-document-text'},
+{name:"Add Balance"},
+{name:"Revert Balance"},
+
+]
+$scope.master=function(delear){
+    console.log(delear);
+    if(delear.name=='Current Balance'){
+        $state.go('currentBalance')
+    }else if(delear.name=='Complain'){
+        $state.go('complain')
+    }else if(delear.name=='Complain List'){
+        $state.go('login')
+    }else if(delear.name=='Change Password'){
+         $state.go('changePassword')
+    }
+    else if(delear.name=='Reports'){
+        $state.go('report')
+    }else if(delear.name=='Add Balance'){
+        $state.go('addBalance')
+    }else if(delear.name=='Revert Balance'){
+        $state.go('revertBalance')
+    }
+}
+
+})
+.controller('reportCtrl',function($scope,$state){
+    $scope.reports=[];
+    $scope.reports=[
+     {name:'Recharge Report',clss:'icon ionIcon ion-document-text'},{name:'Refund Report',clss:'icon ionIcon ion-document-text'},
+     {name:'Topup History',clss:'icon ionIcon ion-document-text'},{name:'Account Report',clss:'icon ionIcon ion-document-text'}
+    ]
+    $scope.repo=function(report){
+      if(report.name=='Recharge Report'){
+          $state.go('rechargeReport')
+      }else if(report.name=='Refund Report'){
+          $state.go('rechargeReport')
+      }else if(report.name=='Topup History'){
+          $state.go('rechargeReport')
+      }else {
+          $state.go('rechargeReport')
+      }
+    }
+})
+.controller('delearCtrl',function($scope,$state){
+    $scope.delear=
+[
+{name:"Current Balance",clas:'icon ionIcon ion-cash'},
+{name:"Complain",clas:'icon ionIcon ion-ios-email'},
+{name:"Complain List",clas:'icon ionIcon ion-ios-email'},
+{name:"Change Password",clas:'icon ionIcon ion-key'},
+{name:"Reports",clas:'icon ionIcon ion-document-text'},
+{name:"Add Balance"},
+{name:"Revert Balance"},
+{name:"Add Retailer"}
+]
+$scope.delr=function(delear){
+    console.log(delear);
+    if(delear.name=='Current Balance'){
+        $state.go('currentBalance')
+    }else if(delear.name=='Complain'){
+        $state.go('complain')
+    }else if(delear.name=='Complain List'){
+        $state.go('login')
+    }else if(delear.name=='Change Password'){
+         $state.go('changePassword')
+    }
+    else if(delear.name=='Reports'){
+        $state.go('report')
+    }else if(delear.name=='Add Balance'){
+        $state.go('addBalance')
+    }else if(delear.name=='Revert Balance'){
+        $state.go('revertBalance')
+    }else if(delear.name=='Add Retailer'){
+        $state.go('addRetailer')
+    }
+}
+})
+.controller('retailerCtrl',function($scope,$state){
+    $scope.retailer=
+[
+{name:"Mobile Recharge",clas:'icon ionIcon ion-android-phone-portrait'},
+{name:"DTH Recharge",clas:'icon ionIcon ion-easel'},
+{name:"Postpaid Recharge",clas:'icon ionIcon ion-android-phone-portrait'},
+{name:"Recharge Transaction",clas:'icon ionIcon ion-document-text'},
+{name:"Search Transaction",clas:'icon ionIcon ion-document-text'},
+{name:"Current Balance",clas:'icon ionIcon ion-cash'},
+{name:"Complain",clas:'icon ionIcon ion-ios-email'},
+{name:"Complain List",clas:'icon ionIcon ion-ios-email'},
+{name:"Change Password",clas:'icon ionIcon ion-key'},
+{name:"Reports",clas:'icon ionIcon ion-document-text'}
+]
+$scope.retlr=function(ret){
+    
+
+if(ret.name=='Mobile Recharge'){
+    $state.go('mobileRecharge')
+}else if(ret.name=='DTH Recharge'){
+    $state.go('dthRecharge')
+}else if(ret.name=='Postpaid Recharge'){
+    $state.go('postPaidRecharge')
+} else if(ret.name=='Recharge Transaction'){
+   $state.go('login');
+}else if(ret.name=='Search Transaction'){
+    $state.go('searchTransaction');
+} else if(ret.name=='Current Balance'){
+        $state.go('currentBalance')
+    }else if(ret.name=='Complain'){
+        $state.go('complain')
+    }else if(ret.name=='Complain List'){
+        $state.go('login')
+    }else if(ret.name=='Change Password'){
+         $state.go('changePassword')
+    }
+    else if(ret.name=='Reports'){
+        $state.go('report')
+    }
+}
+})
+.controller('retailerHomeCtrl',function($scope,$state){
+      $scope.mobileRecharge=
+[
+{name:"Airtel",src:'/img/airtel.jpg'},
+{name:"Vodafone",src:'/img/vodafone.jpg'},
+{name:"Aircel",src:'/img/aircel.jpg'},
+{name:"BSNL-TOPUP",src:'/img/bsnl.jpg'},
+{name:"VIRGIN-GSM",src:'/img/docomo.jpg'},
+{name:"DOCOMO",src:'/img/docomo.jpg'},
+{name:"RECHARGE VIDEOCON",src:'/img/videocon.jpg'},
+{name:"RECHARGE VIDEOCON-SPL",src:'/img/videocon.jpg'},
+{name:"RELIANCE-GSM",src:'/img/relianc.jpg'},
+{name:"RELIANCE-CDMA",src:'../relianc.jpg'},
+{name:"Idea",src:'/img/idea.jpg'},
+{name:"VIRGIN-CDMA",src:'/img/docomo.jpg'},
+{name:"TATA INDICOM",src:'/img/tataInd.jpg'},
+{name:"MTS",src:'/img/mts.jpg'},
+{name:"JIO",src:'/img/jio.jpg'},
+{name:"UNINOR",src:'/img/uninor.jpg'},
+{name:"UNINOR-SPL",src:'/img/uninor.jpg'},
+{name:"BSNL-3G",src:'/img/bsnl.jpg'},
+{name:"BSNL-STV",src:'/img/bsnl.jpg'},
+{name:"DOCOME-SPECIAL",src:'/img/docomo.jpg'},
+{name:"LOOP MOBILE",src:'/img/loop.jpg'},
+{name:"BSNL Recharge",src:'/img/bsnl.jpg'},
+{name:"MTNL-Recharge",src:'/img/img.jpg'},
+{name:"MTNL-TOPUP",src:'/img/img.jpg'},
+{name:"Tata Walky",src:'/img/tataWalky.jpg'},
+
+]
+$scope.dthRecharge=[
+{name:'Dish TV DTH',src:'../dish.png'},
+{name:'Tata Sky DTH',src:'../sky.png'},
+{name:'Big TV DTH',src:'../bigTV.jpg'},
+{name:'Videocon DTH',src:'../videcon.png'},
+{name:'Sun DTH',src:'../sun.png'},
+{name:'Airtel DTH',src:'../airtelDTH.png'}
+]
+$scope.postPaidRecharge=[
+{name:'POSTPAID AIRTEL',src:'../airtel.jpg'},
+{name:'POSTPAID IDEA',src:'../idea.jpg'},
+{name:'POSTPAID VODAFONE',src:'../vodafone.jpg'},
+{name:'POSTPAID BSN',src:'../bsnl.jpg'},
+{name:'POSTPAID DOCOMO',src:'../docomo.jpg'},
+{name:'POSTPAID RELICE CDMA/GSM',src:'../relianc.jpg'}
+]
+$scope.mobilRecg=function(){
+    $state.go('recharge')
+
+
+}
+})
